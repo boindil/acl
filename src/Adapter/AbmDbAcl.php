ac286e257de4adc60f35b793d671193e9d6d4dd3
root <root@bighp.de>
2014-12-29 00:56:11 +0100

    Apply patches from master

--- src/Adapter/AbmDbAcl.php
+++ src/Adapter/AbmDbAcl.php
@@ -4,11 +4,15 @@
 	use Cake\Core\App;
 	use Cake\ORM\TableRegistry;
 	class AbmDbAcl extends CachedDbAcl {
-	  public function __construct() {
-		  $config = ['className' => App::className('Acl.AbmPermissionsTable', 'Model/Table')];
-		  $this->Permission = TableRegistry::get('Permissions', $config);
-		  $this->Aro = $this->Permission->Aros->target();
-		  $this->Aco = $this->Permission->Acos->target();
-	  }
+		public function __construct() {
+			$config = ['className' => App::className('Acl.AbmPermissionsTable', 'Model/Table')];
+			$this->Permission = TableRegistry::get('Permissions', $config);
+			$this->Aro = $this->Permission->Aros->target();
+			$this->Aco = $this->Permission->Acos->target();
+		}
+		
+		public function check($aro, $aco, $action = "*", $overwrite = true) {
+			return $this->Permission->check($aro, $aco, $action, $overwrite);
+		}
 	}
 ?>
